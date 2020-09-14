ac576470366ccaa96c3d3d5ffda9bc6b56d22a6b
hillarioh <okerioh@gmail.com>
2020-09-14 07:24:48 +0300

    setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+   entry: path.join(__dirname, 'src', 'app.jsx'),
+   output: {
+	filename: 'bundle.js',
+	path: path.resolve(__dirname),           
+   },
+   module: {
+       rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+   },
+   devtool: 'source-map',
+   resolve: {
+     extensions: ['.js','.jsx','*']
+   },
+};
