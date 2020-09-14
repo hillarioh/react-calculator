414be4ef4ce135a0d239cf4e3ad5c5a4c33a16f7
hillarioh <okerioh@gmail.com>
2020-09-14 13:15:57 +0300

    add event listener functions

--- src/app.jsx
+++ src/app.jsx
@@ -10,66 +10,22 @@ class CalculatorContainer extends React.Component {
             num1: '',
             num2: ''
         }
-        this.setNum1 = this.setNum1.bind(this);
-        this.setNum2 = this.setNum2.bind(this);
-        this.add = this.add.bind(this);
-        this.subtract = this.subtract.bind(this);
-        this.multiply = this.multiply.bind(this);
-        this.divide = this.divide.bind(this);
-        this.clear = this.clear.bind(this);
+        this.setNum = this.setNum.bind(this);
     }
 
-    setNum1(e){
+    setNum(input){
         this.setState({
-            num1: e.target.value
         });
-    }
+    }   
 
-    setNum2(e){
-        this.setState({
-            num2: e.target.value
-        });
-    }
+   
 
-    add(){
-        this.setState({
-            result: parseInt(this.state.num1) + parseInt(this.state.num2)
-        });
-    }
-    subtract(){
-        this.setState({
-            result: parseInt(this.state.num1) - parseInt(this.state.num2)
-        });
-    }
-
-    multiply(){
-        this.setState({
-            result: parseInt(this.state.num1) * parseInt(this.state.num2)
-        });
-    }
-
-
-
-    divide(event){
-        event.preventDefault();
-        this.setState({
-            result: parseFloat(this.state.num1) / parseFloat(this.state.num2)
-        });
-    }
-
-    clear(e){
-        e.preventDefault();
-        this.setState({
-            result: 0,
-            num1: '',
-            num2: ''
-        });
-    }
+   
 
     render(){
         let {num1,num2} = this.state;
         return (
-           <CalcUi />
+           <CalcUi  />
          
         );
     }
--- src/calculator.jsx
+++ src/calculator.jsx
@@ -2,38 +2,103 @@ import React from 'react';
 
 
 export class CalcUi extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            input: '',
+            result: 0
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.posOrNeg = this.posOrNeg.bind(this);
+        this.clear = this.clear.bind(this);
+        this.add = this.add.bind(this);
+        this.subtract = this.subtract.bind(this);
+        this.multiply = this.multiply.bind(this);
+        this.divide = this.divide.bind(this);
+        this.percentage = this.percentage.bind(this);
+    }
+
+    handleInput(e){
+        this.setState({
+            input: e.target.id
+        })
+    }
+
+    clear(){
+        this.setState({
+            result: 0
+        })
+    }
+
+    posOrNeg(){}
+
+    percentage(){}
+
+    add(){
+        this.setState({
+           
+        });
+    }
+
+    subtract(){
+        this.setState({
+          
+        });
+    }
+
+    multiply(){
+        this.setState({
+           
+        });
+    }
+
+    divide(){
+        this.setState({
+           
+        });
+    }
+
+    getResult(){
+
+    }
+
+
+
+
     render(){
         return(
-            <div id="calc">
-                <div className="result">NaN</div>
-                <div className="col-4 r1">                    
-                    <p id="ac">AC</p>
-                    <p id="p-n">+/-</p>
-                    <p id="per">%</p>
-                    <p id="div">/</p>
-                </div>
-                <div className="col-4 r2">
-                    <p id="seven">7</p>
-                    <p id="eight">8</p>
-                    <p id="nine">9</p>
-                    <p id="div">X</p>
-                </div>
-                <div className="col-4 r3">
-                    <p id="four">4</p>
-                    <p id="five">5</p>
-                    <p id="six">6</p>
-                    <p id="sub">-</p>
-                </div>
-                <div className="col-4 r4">
-                    <p id="one">1</p>
-                    <p id="two">2</p>
-                    <p id="three">3</p>
-                    <p id="plus">+</p>
-                </div>
-                <div className="col-3 r5">
-                    <p id="zero">0</p>
-                    <p id="point">.</p>
-                    <p id="equal">=</p>
+            <div>
+                <div id="calc">
+                    <div className="result"><p>{this.state.result}</p></div>
+                    <div className="col-4 r1">                    
+                        <p id="ac" onClick={this.clear}>AC</p>
+                        <p id="p-n" onClick={this.posOrNeg}>+/-</p>
+                        <p id="per" onClick={this.percentage}>%</p>
+                        <p id="div" onClick={this.divide}>/</p>
+                    </div>
+                    <div className="col-4 r2">
+                        <p id="seven" onClick={this.handleInput}>7</p>
+                        <p id="eight" onClick={this.handleInput}>8</p>
+                        <p id="nine" onClick={this.handleInput}>9</p>
+                        <p id="div" onClick={this.multiply}>X</p>
+                    </div>
+                    <div className="col-4 r3">
+                        <p id="four" onClick={this.handleInput}>4</p>
+                        <p id="five" onClick={this.handleInput}>5</p>
+                        <p id="six" onClick={this.handleInput}>6</p>
+                        <p id="sub" onClick={this.subtract}>-</p>
+                    </div>
+                    <div className="col-4 r4">
+                        <p id="one" onClick={this.handleInput}>1</p>
+                        <p id="two" onClick={this.handleInput}>2</p>
+                        <p id="three" onClick={this.handleInput}>3</p>
+                        <p id="plus" onClick={this.add}>+</p>
+                    </div>
+                    <div className="col-3 r5">
+                        <p id="zero" onClick={this.handleInput}>0</p>
+                        <p id="point" onClick={this.handleInput}>.</p>
+                        <p id="equal" onClick={this.getResult}>=</p>
+                    </div>
                 </div>
             </div>
         );
